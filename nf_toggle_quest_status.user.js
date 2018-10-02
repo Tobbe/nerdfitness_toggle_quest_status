1fde226c89b354f0537dac3b76beb4551503baf9
Tobbe Lundberg <tobbe@tlundberg.com>
2018-10-02 22:10:18 +0200

    Initial commit

---
+++ nf_toggle_quest_status.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         NF Toggle Quest Status
+// @namespace    https://github.com/tobbe
+// @version      0.1
+// @description  Toggle quest status by clicking on icon
+// @license      MIT
+// @author       Tobbe
+// @match        https://www.nerdfitness.com/level-up/my-quests/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let count = 0;
+
+    setTimeout(() => {
+        document.querySelectorAll('.qh-right .qh-rect img').forEach(checkmark => {
+            count++;
+            checkmark.onclick = () => {
+                console.log('click');
+            };
+        });
+
+        console.log('found', count, 'checkmarks');
+    }, 3000);
+})();
